0d16d97c7acde7e3bc2fca8224cf7cdde6c843bd
spagnuolo <spagnuol@stud.fra-uas.de>
2021-01-26 14:50:08 +0100

    change title

--- src/Title.svelte
+++ src/Title.svelte
@@ -1,9 +1,21 @@
+<h1>Meinzeit</h1>
+<h2>untertitel teest</h2>
+<p><span>Ausgabe 2030/2</span> <span class="preis">$1,30</span></p>
+<hr />
+
 <style>
     h1 {
         font-family: Georgia, "Times New Roman", Times, serif;
         font-size: 60px;
         text-align: center;
         text-transform: uppercase;
+        margin: 16px 0 0 0;
+    }
+
+    h2 {
+        font-family: Georgia, "Times New Roman", Times, serif;
+        font-size: 40px;
+        text-align: center;
         margin: 16px 0 0 0;
     }
 
@@ -20,7 +32,3 @@
         margin-right: 10px;
     }
 </style>
-
-<h1>MMXXX - AA Heute</h1>
-<p><span>Ausgabe 2030/2</span> <span class="preis">$1,30</span></p>
-<hr />
